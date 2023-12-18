523bcb4399399c0d6dc4e59c6a711af9e33822bf
Aman Seth (CMP - Student) <zfb23ecu@UEA.AC.UK>
2023-12-18 00:33:25 +0000

    Removed unwanted comments from main.py

--- Scripts/main.py
+++ Scripts/main.py
@@ -23,15 +23,3 @@ class HouseOfTheDead(object):
 if __name__ == "__main__":
     obj = HouseOfTheDead()
     obj.main()
-
-"""
-Code references:-
-Colour text: https://ozzmaker.com/add-colour-to-text-in-python/
-Progress Bar: https://medium.com/pythoniq/progress-bars-in-python-with-progressbar2-da77838077a9
-Move Counter: https://stackoverflow.com/questions/54714945/create-a-function-that-will-increment-by-one-when-called
-csv writer / reader: https://docs.python.org/3/library/csv.html
-datetime: https://stackoverflow.com/questions/415511/how-do-i-get-the-current-time
-Fonts: https://patorjk.com/software/taag/#p=testall&h=1&v=3&f=Ghost&t=House%20of%20the%20Dead
-csv dict reader: https://www.geeksforgeeks.org/load-csv-data-into-list-and-dictionary-using-python/
-re module: https://www.geeksforgeeks.org/python-extract-numbers-from-string/
-"""
